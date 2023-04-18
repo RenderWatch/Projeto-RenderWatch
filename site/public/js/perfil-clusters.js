56a2ad6916c54c754fdaf4ee7f23ddfd03f72206
clarasalomaofaria <clara.faria@sptech.school>
2023-04-18 14:29:20 -0300

    Estilizações básicas da página clusters

--- site/public/js/perfil-clusters.js
+++ site/public/js/perfil-clusters.js
@@ -12,16 +12,18 @@ function buscarDadosCluster() {
                 for (let i = 0; i < resposta.length; i++) {
 
                     tabelaContainer.innerHTML += `
+                    <span class="info-titulo-cluster">
                     <div id='nomeCluster${resposta[i].id}' class='nome-cluster'>
-                        ${resposta[i].nome}
+                    ${resposta[i].nome}
                     </div>
-                    <span id='containerBtn${resposta[i].id}'>
+                    <span id='containerBtn${resposta[i].id}' class="info-btn-cluster">
                     <button onclick="alterarNomeCluster(${resposta[i].id})" id="btnEditNomeCluster${resposta[i].id}" class="btn-edit">
-                    Editar Nome
+                    Editar
                     </button>
-                    <button onclick="deletarCluster(${resposta[i].id})" id="btnDeletarCluster${resposta[i].id}" class="btn-edit">
-                    Deletar Cluster
+                    <button onclick="deletarCluster(${resposta[i].id})" id="btnDeletarCluster${resposta[i].id}" class="btn-edit-vermelho">
+                    Deletar
                     </button>
+                    </span>
                     </span>
                     <div id="containerCluster${resposta[i].id}">
                         <table id='tabelaCluster${resposta[i].id}' class="tabela-cluster">
@@ -31,10 +33,13 @@ function buscarDadosCluster() {
                                 <th>Métrica CPU</th>
                                 <th>Métrica Disco</th>
                                 <th>Métrica Memória</th>
+                                <th>Editar</th>
                             </tr>
                         </table>
+                        <span class="container-btn-add-maquina">
                         <button id='btnAddMaquinaCluster${i + 1}' class='btn-add-maquina' onclick='adicionarMaquina(${resposta[i].id})'>Adicionar Máquina</button>
-                    </div>
+                        </span>
+                        </div>
                                     `
                     var idCluster = resposta[i].id
 
@@ -71,27 +76,27 @@ function buscarDadosMaquina(idCluster) {
                                 <td>${resposta[i].metrica_cpu}%</td>
                                 <td>${resposta[i].metrica_disco}%</td>
                                 <td>${resposta[i].metrica_memoria}%</td>
-                            </tr>
-                            <span id="containerEditInfoMaquina${resposta[i].id}">
+                                </tr>
+                                 <div id="containerEditInfoMaquina${resposta[i].id}" class="btn-maquina">
                                 <button onclick="alterarInfoMaquina(
-                                    ${resposta[i].id},
-                                    '${resposta[i].nome}',
-                                      ${resposta[i].metrica_cpu},
-                                       ${resposta[i].metrica_disco},
-                                        ${resposta[i].metrica_memoria}
-                                    )
-                                        " id="btnEditInfoMaquina${resposta[i].id}" class="btn-edit">
-                                    Editar
+                                ${resposta[i].id},
+                                '${resposta[i].nome}',
+                                ${resposta[i].metrica_cpu},
+                                ${resposta[i].metrica_disco},
+                                ${resposta[i].metrica_memoria}
+                                )
+                                " id="btnEditInfoMaquina${resposta[i].id}" class="btn-edit-maquina">
+                                         Editar
                                 </button>
                                 <button onclick="deletarMaquina(
-                                    ${resposta[i].id}
-                                    )
-                                        " id="btnDeleteMaquina${resposta[i].id}" class="btn-edit">
+                                ${resposta[i].id}
+                                )
+                                " id="btnDeleteMaquina${resposta[i].id}" class="btn-edit-maquina-vermelho">
                                     Deletar
                                 </button>
-                            </span>
-                        `
-                    }
+                            </div>
+                                `
+                            }
                 });
             }
         } else {
@@ -112,12 +117,12 @@ function alterarInfoMaquina(idMaquina, nome, metrica_cpu, metrica_disco, metrica
     <td><input id="inputMetricaMemoriaMaquina${idMaquina}" class="input-maquina-info" type="number" value="${metrica_memoria}" oninput="if (this.value.length > 2) this.value = this.value.slice(0, 2);"></td>
     `
     document.getElementById(`containerEditInfoMaquina${idMaquina}`).innerHTML = `
-    <button onclick="confirmarAlteracaoInfoMaquina(${idMaquina})" class="btn-maquina-info">Confirmar</button>
+    <button onclick="confirmarAlteracaoInfoMaquina(${idMaquina})" class="btn-edit-maquina">Confirmar</button>
     <button onclick="cancelaAlteracaoInfoMaquina( ${idMaquina},
         '${nome}',
           ${metrica_cpu},
            ${metrica_disco},
-            ${metrica_memoria})" class="btn-maquina-info-cancelar">Cancelar</button>
+            ${metrica_memoria})" class="btn-edit-maquina-vermelho">Cancelar</button>
     `
 }
 
@@ -143,6 +148,12 @@ function cancelaAlteracaoInfoMaquina(idMaquina, nome, metrica_cpu, metrica_disco
             " id="btnEditInfoMaquina${idMaquina}" class="btn-edit">
         Editar
     </button>
+    <button onclick="deletarMaquina(
+        ${idMaquina}
+        )
+        " id="btnDeleteMaquina${idMaquina}" class="btn-edit">
+            Deletar
+        </button>
     `
 
 }
@@ -170,11 +181,6 @@ function confirmarAlteracaoInfoMaquina(idMaquina) {
         }).then(function (resposta) {
 
             if (resposta.ok) {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Parabéns',
-                    text: 'Informações da máquina atualizadas com sucesso!',
-                })
 
                 location.reload();
 
@@ -234,7 +240,7 @@ function adicionarCluster() {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
-                text: 'Houve um erro ao tentar realizar o cadastro!',
+                text: 'Houve um erro ao tentar adicionar um cluster!',
             })
 
         }
@@ -245,18 +251,6 @@ function adicionarCluster() {
     return false;
 
 }
-
-
-function alterarNomeCluster(clusterId) {
-    document.getElementById(`containerBtn${clusterId}`).innerHTML = `
-      <div id="divChangeNomeCluster" class="div-input">
-          <input class="input-cluster-info" id="inputNomeCluster${clusterId}" type="text">
-          <button onclick="confirmarNomeCluster(${clusterId}, inputNomeCluster${clusterId})" class="btn-cluster-info">Confirmar</button>
-          <button onclick="cancelarNomeCluster(${clusterId})" class="btn-cluster-info-cancelar">Cancelar</button>
-      </div>
-      `;
-}
-
 
 function adicionarMaquina(clusterId) {
     fetch("/cluster/adicionarMaquina", {
@@ -294,8 +288,8 @@ function alterarNomeCluster(clusterId) {
     document.getElementById(`containerBtn${clusterId}`).innerHTML = `
       <div id="divChangeNomeCluster" class="div-input">
           <input class="input-cluster-info" id="inputNomeCluster${clusterId}" type="text">
-          <button onclick="confirmarNomeCluster(${clusterId}, inputNomeCluster${clusterId})" class="btn-cluster-info">Confirmar</button>
-          <button onclick="cancelarNomeCluster(${clusterId})" class="btn-cluster-info-cancelar">Cancelar</button>
+          <button onclick="confirmarNomeCluster(${clusterId}, inputNomeCluster${clusterId})" class="btn-edit">Confirmar</button>
+          <button onclick="cancelarNomeCluster(${clusterId})" class="btn-edit-vermelho">Cancelar</button>
       </div>
       `;
 }
@@ -306,6 +300,9 @@ function cancelarNomeCluster(clusterId) {
     document.getElementById(`containerBtn${clusterId}`).innerHTML = `
       <button onclick="alterarNomeCluster(${clusterId})" id="btnEditNomeCluster${clusterId}" class="btn-edit">
           Editar
+      </button>
+      <button onclick="deletarCluster(${clusterId})" id="btnDeletarCluster${clusterId}" class="btn-edit-vermelho">
+                    Deletar Cluster
       </button>
       `;
 }
@@ -465,16 +462,25 @@ function deletarClusterSemMaquina(clusterId) {
 }
 
 function deletarCluster(clusterId) {
-    fetch(`/cluster/deletarCluster/${clusterId}`).then(function (resposta) {
-        if (resposta.ok) {
-            if (resposta.status == 200) {
-                deletarMaquinaDoCluster(clusterId);
-                console.log('deu 200')
-            } else {
-                deletarClusterSemMaquina(clusterId);
-            }
-            
-            
+
+    Swal.fire({
+        title: 'Tem certeza de que você quer apagar este cluster?',
+        text: "",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            fetch(`/cluster/deletarCluster/${clusterId}`).then(function (resposta) {
+                if (resposta.ok) {
+                    if (resposta.status == 200) {
+                        deletarMaquinaDoCluster(clusterId);
+                        console.log('deu 200')
+                    } else {
+                        deletarClusterSemMaquina(clusterId);
+                    }
         } else {
             throw ('Houve um erro na API!');
         }
@@ -482,9 +488,26 @@ function deletarCluster(clusterId) {
         console.error(resposta);
     });
     
+    }
+    
+    })
+    
 }
 
 function deletarMaquina(idMaquina) {
+
+    
+    Swal.fire({
+        title: 'Tem certeza de que você quer apagar esta máquina?',
+        text: "",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim'
+      }).then((result) => {
+        if (result.isConfirmed) {
+        
     fetch(`/cluster/deletarMaquina/${idMaquina}`, {
         method: "DELETE",
         headers: {
@@ -514,6 +537,10 @@ function deletarMaquina(idMaquina) {
     });
 
     return false;
+    
+    }
+    
+    })
 }
 
 buscarDadosCluster();
