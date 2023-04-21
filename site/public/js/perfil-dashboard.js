5807c6ad16c0ba15ec804ec2ec3b87d9f73e77cc
FerArj <fernando.araujo@sptech.school>
2023-04-21 11:29:20 -0300

    mais alterações

--- site/public/js/perfil-dashboard.js
+++ site/public/js/perfil-dashboard.js
@@ -29,7 +29,7 @@ const ramChart = new Chart(ram, {
 
 const discoData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
 
-new Chart(disco, {
+const discoChart = new Chart(disco, {
     type: 'line',
     data: {
         labels: ['13:00:15', '13:00:30', '13:00:45', '13:01:00', '13:01:15', '13:01:30', '13:01:45', '13:02:00', '13:02:15', '13:02:30', '13:02:45'],
@@ -50,7 +50,7 @@ new Chart(disco, {
 
 const cpuData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
 
-new Chart(cpu, {
+const cpuChart = new Chart(cpu, {
     type: 'line',
     data: {
         labels: ['13:00:15', '13:00:30', '13:00:45', '13:01:00', '13:01:15', '13:01:30', '13:01:45', '13:02:00', '13:02:15', '13:02:30', '13:02:45'],
@@ -71,7 +71,7 @@ new Chart(cpu, {
 
 const redeData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
 
-new Chart(rede, {
+const redeChart = new Chart(rede, {
     data: {
         labels: ['13:00:15', '13:00:30', '13:00:45', '13:01:00', '13:01:15', '13:01:30', '13:01:45', '13:02:00', '13:02:15', '13:02:30', '13:02:45'],
         datasets: [{
@@ -89,3 +89,45 @@ new Chart(rede, {
         }
     }
 });
+
+
+function atualizarDados() {
+  // Gerar novos valores aleatórios para cada gráfico
+  const novoRamData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+  const novoDiscoData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+  const novoCpuData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+  const novoRedeData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+
+  // Atualizar os dados dos gráficos existentes com os novos valores
+  ramChart.data.datasets[0].data = novoRamData;
+  ramChart.update();
+  discoChart.data.datasets[0].data = novoDiscoData;
+  discoChart.update();
+  cpuChart.data.datasets[0].data = novoCpuData;
+  cpuChart.update();
+  redeChart.data.datasets[0].data = novoRedeData;
+  redeChart.update();
+}
+
+// Atualizar os dados a cada 5 segundos
+setInterval(atualizarDados, 15000);
+
+
+// Adiciona um event listener para o botão de gerar relatório
+const botaoGerarRelatorio = document.getElementById('botao-gerar-relatorio');
+botaoGerarRelatorio.addEventListener('click', gerarRelatorio);
+
+// Função para gerar o relatório
+function gerarRelatorio() {
+  // Cria um novo documento PDF
+  const doc = new jsPDF();
+
+  // Adiciona os gráficos ao documento
+  doc.addImage(ramChart.toBase64Image(), 'JPEG', 15, 15, 80, 40);
+  doc.addImage(discoChart.toBase64Image(), 'JPEG', 15, 70, 80, 40);
+  doc.addImage(cpuChart.toBase64Image(), 'JPEG', 15, 125, 80, 40);
+  doc.addImage(redeChart.toBase64Image(), 'JPEG', 15, 180, 80, 40);
+
+  // Salva o PDF
+  doc.save('relatorio.pdf');
+}
