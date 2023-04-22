56db0aff63cfaef81e61e5512dfa42971df1cc0e
FerArj <fernando.araujo@sptech.school>
2023-04-22 16:17:43 -0300

    botoes de relatorio

--- site/public/js/perfil-dashboard.js
+++ site/public/js/perfil-dashboard.js
@@ -8,23 +8,41 @@ const ramData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) 
 
 // Criar o gráfico de RAM com os dados aleatórios
 const ramChart = new Chart(ram, {
-  type: 'line',
-  data: {
-    labels: ['13:00:15', '13:00:30', '13:00:45', '13:01:00', '13:01:15', '13:01:30', '13:01:45', '13:02:00', '13:02:15', '13:02:30', '13:02:45'],
-    datasets: [{
-      type: 'line',
-      label: 'Uso de memoria RAM',
-      data: ramData,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+    type: 'line',
+    data: {
+        labels: ['13:00:15', '13:00:30', '13:00:45', '13:01:00', '13:01:15', '13:01:30', '13:01:45', '13:02:00', '13:02:15', '13:02:30', '13:02:45'],
+        datasets: [{
+            type: 'line',
+            label: 'Uso de memoria RAM',
+            data: ramData,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    callback: function (value, index, values) {
+                        return value + ' GB';
+                    }
+                }
+            }
+        },
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        return context.dataset.label + ': ' + context.parsed.y + ' GB';
+                    }
+                }
+            }
+        },
+        animation: {
+            duration: 2000, // duração da animação em milissegundos
+            easing: 'easeInOutQuart' // curva de animação
+        }
     }
-  }
 });
 
 const discoData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
@@ -44,6 +62,10 @@ const discoChart = new Chart(disco, {
             y: {
                 beginAtZero: true
             }
+        },
+        animation: {
+            duration: 2000, // duração da animação em milissegundos
+            easing: 'easeInOutQuart' // curva de animação
         }
     }
 });
@@ -65,6 +87,10 @@ const cpuChart = new Chart(cpu, {
             y: {
                 beginAtZero: true
             }
+        },
+        animation: {
+            duration: 2000, // duração da animação em milissegundos
+            easing: 'easeInOutQuart' // curva de animação
         }
     }
 });
@@ -86,32 +112,35 @@ const redeChart = new Chart(rede, {
             y: {
                 beginAtZero: true
             }
+        },
+        animation: {
+            duration: 2000, // duração da animação em milissegundos
+            easing: 'easeInOutQuart' // curva de animação
         }
     }
 });
 
 
 function atualizarDados() {
-  // Gerar novos valores aleatórios para cada gráfico
-  const novoRamData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
-  const novoDiscoData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
-  const novoCpuData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
-  const novoRedeData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+    // Gerar novos valores aleatórios para cada gráfico
+    const novoRamData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+    const novoDiscoData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+    const novoCpuData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
+    const novoRedeData = Array.from({ length: 11 }, () => Math.floor(Math.random() * 10) + 1);
 
-  // Atualizar os dados dos gráficos existentes com os novos valores
-  ramChart.data.datasets[0].data = novoRamData;
-  ramChart.update();
-  discoChart.data.datasets[0].data = novoDiscoData;
-  discoChart.update();
-  cpuChart.data.datasets[0].data = novoCpuData;
-  cpuChart.update();
-  redeChart.data.datasets[0].data = novoRedeData;
-  redeChart.update();
+    // Atualizar os dados dos gráficos existentes com os novos valores
+    ramChart.data.datasets[0].data = novoRamData;
+    ramChart.update();
+    discoChart.data.datasets[0].data = novoDiscoData;
+    discoChart.update();
+    cpuChart.data.datasets[0].data = novoCpuData;
+    cpuChart.update();
+    redeChart.data.datasets[0].data = novoRedeData;
+    redeChart.update();
 }
 
 // Atualizar os dados a cada 5 segundos
 setInterval(atualizarDados, 15000);
-
 
 // Adiciona um event listener para o botão de gerar relatório
 const botaoGerarRelatorio = document.getElementById('botao-gerar-relatorio');
@@ -119,15 +148,15 @@ botaoGerarRelatorio.addEventListener('click', gerarRelatorio);
 
 // Função para gerar o relatório
 function gerarRelatorio() {
-  // Cria um novo documento PDF
-  const doc = new jsPDF();
+    // Cria um novo documento PDF
+    const doc = new jsPDF();
 
-  // Adiciona os gráficos ao documento
-  doc.addImage(ramChart.toBase64Image(), 'JPEG', 15, 15, 80, 40);
-  doc.addImage(discoChart.toBase64Image(), 'JPEG', 15, 70, 80, 40);
-  doc.addImage(cpuChart.toBase64Image(), 'JPEG', 15, 125, 80, 40);
-  doc.addImage(redeChart.toBase64Image(), 'JPEG', 15, 180, 80, 40);
+    // Adiciona os gráficos ao documento
+    doc.addImage(ramChart.toBase64Image(), 'JPEG', 15, 15, 80, 40);
+    doc.addImage(discoChart.toBase64Image(), 'JPEG', 15, 70, 80, 40);
+    doc.addImage(cpuChart.toBase64Image(), 'JPEG', 15, 125, 80, 40);
+    doc.addImage(redeChart.toBase64Image(), 'JPEG', 15, 180, 80, 40);
 
-  // Salva o PDF
-  doc.save('relatorio.pdf');
+    // Salva o PDF
+    doc.save('relatorio.pdf');
 }
