41824e48ff1f071d7cf79dbea2a4b6f51b3700ed
clarasalomaofaria <clara.faria@sptech.school>
2023-04-24 12:26:00 -0300

    Adição do server da azure em config.js

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -3,10 +3,10 @@ var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
 var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    server: "svr-grupo6.database.windows.net",
+    database: "render_watch",
+    user: "adm-render-watch",
+    password: "#Gfgrupo6",
     pool: {
         max: 10,
         min: 0,
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "renderwatch",
     user: "root",
-    password: "#Gf12431771601",
+    password: "admin",
 };
 
 function executar(instrucao) {
