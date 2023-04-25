e7b177632d1cb246997cd7c829bf760f49d8f1ae
clarasalomaofaria <clara.faria@sptech.school>
2023-04-25 18:51:01 -0300

    Alteração na tela de download

--- site/public/js/perfil-download.js
+++ site/public/js/perfil-download.js
@@ -1,26 +1,21 @@
 
 function copyCode() {
-    // Get the code element
+
     const codeElement = document.querySelector('.code');
-  
-    // Create a temporary textarea element and set its value to the code
+
     const textArea = document.createElement('textarea');
     textArea.value = codeElement.innerText;
-  
-    // Add the textarea to the DOM and select its contents
+
     document.body.appendChild(textArea);
     textArea.select();
-  
-    // Copy the code to the clipboard
+
     document.execCommand('copy');
-  
-    // Remove the textarea from the DOM
+
     document.body.removeChild(textArea);
-  
-    // Show the pop-up message
-    const popup = document.querySelector('.popup');
-    popup.style.display = 'block';
-    setTimeout(() => {
-      popup.style.display = 'none';
-    }, 2000);
+
+    // const popup = document.querySelector('.popup');
+    // popup.style.display = 'block';
+    // setTimeout(() => {
+    //   popup.style.display = 'none';
+    // }, 2000);
   }
