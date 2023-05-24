f1fe41d4649f6ad30784f3f4c553b11b91aac5ee
FilipeRicardoSilva <filipe.silva@sptech.school>
2023-05-24 19:58:14 -0300

    modelAlerta

--- site/src/models/alertasModel.js
+++ site/src/models/alertasModel.js
@@ -4,7 +4,7 @@ function buscarDadosCluster(idEmpresa) {
     console.log("ACESSEI O ALERTA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
     SELECT *
-    FROM cluster 
+    FROM historicoAlertas 
     WHERE empresa_id = '${idEmpresa}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
