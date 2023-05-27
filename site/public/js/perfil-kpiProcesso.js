d82e1812a7c900efa8869ebd461e909a068baef9
AndreSingillo2000 <andre.araujo@sptech.school>
2023-05-27 18:30:37 -0300

    criação controller, model, perfil, route kpiProcesso.js, desenvolvimento de código e mapeamento

---
+++ site/public/js/perfil-kpiProcesso.js
@@ -0,0 +1,183 @@
+
+var idMaquina = sessionStorage.MAQUINA;
+var idCluster = sessionStorage.CLUSTER;
+function atualizarDadosProcesso() {
+    fetch(`/kpiProcesso/listarProcessos/${idMaquina}`)
+        .then(function (resposta) {
+            if (resposta.ok) {
+                resposta.json().then(function (resposta) {
+                    console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+                    var feed = document.getElementById("feed_container");
+                    feed.innerHTML = "";
+
+                    if (resposta.length > 0) {
+                        for (let i = 0; i < resposta.length; i++) {
+                            const dados = resposta[i];
+                            const total_processos = dados.total_processos;
+                            const total_threads = dados.total_threads;
+                        
+                            document.getElementById("total-processos").innerHTML = `<span>${total_processos}</span>`;
+                            document.getElementById("total-threads").innerHTML = `<span>${total_threads}</span>`;
+                    
+                        }
+                    } else {
+                        var mensagem = document.createElement("span");
+                        mensagem.innerHTML = "Nenhum resultado encontrado.";
+                        feed.appendChild(mensagem);
+                        throw "Nenhum resultado encontrado!";
+                    }
+
+                    
+
+                });
+            } else {
+                throw "Houve um erro na API!";
+            }
+        })
+        .catch(function (resposta) {
+            console.error(resposta);
+        });
+}
+
+function listarAlertaCluster() {
+    fetch(`/kpiProcesso/listarAlertaCluster/${idCluster}`)
+        .then(function (resposta) {
+            if (resposta.ok) {
+                resposta.json().then(function (resposta) {
+                    console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+                    var feed = document.getElementById("feed_container");
+                    feed.innerHTML = "";
+
+                    if (resposta.length > 0) {
+                        for (let i = 0; i < resposta.length; i++) {
+                            const dados = resposta[i];
+                            const quantidade_alertas = dados.quantidade_alertas;
+    
+                            // Inserir os valores nos elementos HTML correspondentes
+                            document.getElementById("span_alertas_cluster").innerHTML = `<span>${quantidade_alertas}</span>`;
+                        }
+                    } else {
+                        var mensagem = document.createElement("span");
+                        mensagem.innerHTML = "Nenhum resultado encontrado.";
+                        feed.appendChild(mensagem);
+                        throw "Nenhum resultado encontrado!";
+                    }
+                    
+                });
+            } else {
+                throw "Houve um erro na API!";
+            }
+        })
+        .catch(function (resposta) {
+            console.error(resposta);
+        });
+}
+
+function listarAlertaMaquina() {
+    fetch(`/kpiProcesso/listarAlertaMaquina/${idMaquina}`)
+        .then(function (resposta) {
+            if (resposta.ok) {
+                resposta.json().then(function (resposta) {
+                    console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+                    var feed = document.getElementById("feed_container");
+                    feed.innerHTML = "";
+
+                    if (resposta.length > 0) {
+                        for (let i = 0; i < resposta.length; i++) {
+                            const dados = resposta[i];
+                            const quantidade_alertas = dados.quantidade_alertas;
+    
+                            // Inserir os valores nos elementos HTML correspondentes
+                            document.getElementById("span_alertas_maquina").innerHTML = `<span>${quantidade_alertas}</span>`;
+                        }
+                    } else {
+                        var mensagem = document.createElement("span");
+                        mensagem.innerHTML = "Nenhum resultado encontrado.";
+                        feed.appendChild(mensagem);
+                        throw "Nenhum resultado encontrado!";
+                    }
+                    
+                });
+            } else {
+                throw "Houve um erro na API!";
+            }
+        })
+        .catch(function (resposta) {
+            console.error(resposta);
+        });
+}
+
+function listarAlertaComponenteMaquina() {
+    fetch(`/kpiProcesso/listarAlertaComponenteMaquina/${idMaquina}`)
+        .then(function (resposta) {
+            if (resposta.ok) {
+                resposta.json().then(function (resposta) {
+                    console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+                    var feed = document.getElementById("feed_container");
+                    feed.innerHTML = "";
+
+                    if (resposta.length > 0) {
+                        for (let i = 0; i < resposta.length; i++) {
+                            const dados = resposta[i];
+                            const nome_componente = dados.nome_componente;
+    
+                            // Inserir os valores nos elementos HTML correspondentes
+                            document.getElementById("componente_alerta").innerHTML = `<span>${nome_componente}</span>`;
+
+                        }
+                    } else {
+                        var mensagem = document.createElement("span");
+                        mensagem.innerHTML = "Nenhum resultado encontrado.";
+                        feed.appendChild(mensagem);
+                        throw "Nenhum resultado encontrado!";
+                    }
+                    
+                });
+            } else {
+                throw "Houve um erro na API!";
+            }
+        })
+        .catch(function (resposta) {
+            console.error(resposta);
+        });
+}
+
+function listarMaquinaMaiorAlertas() {
+    fetch(`/kpiProcesso/listarMaquinaMaiorAlertas`)
+        .then(function (resposta) {
+            if (resposta.ok) {
+                resposta.json().then(function (resposta) {
+                    console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+                    var feed = document.getElementById("feed_container");
+                    feed.innerHTML = "";
+
+                    if (resposta.length > 0) {
+                        for (let i = 0; i < resposta.length; i++) {
+                            const dados = resposta[i];
+                            const nome_maquina = dados.nome_maquina;
+                           
+                            // Inserir os valores nos elementos HTML correspondentes
+                            document.getElementById("span_alertas_maquina").innerHTML = `<span>${nome_maquina}</span>`;
+                            
+                        }
+                    } else {
+                        var mensagem = document.createElement("span");
+                        mensagem.innerHTML = "Nenhum resultado encontrado.";
+                        feed.appendChild(mensagem);
+                        throw "Nenhum resultado encontrado!";
+                    }
+                    
+                });
+            } else {
+                throw "Houve um erro na API!";
+            }
+        })
+        .catch(function (resposta) {
+            console.error(resposta);
+        });
+}
--- site/public/js/perfil-redeMaquina.js
+++ site/public/js/perfil-redeMaquina.js
@@ -19,6 +19,7 @@ function atualizarDadosCluster() {
 
                     const dados = resposta[0];
                     idCluster = dados.id;
+                    sessionStorage.CLUSTER = dados.id;
 
                     var feed = document.getElementById("feed_container");
                     feed.innerHTML = "";
@@ -170,45 +171,7 @@ function atualizarDadosRede() {
                         feed.appendChild(mensagem);
                         throw "Nenhum resultado encontrado!";
                     }
-                });
-            } else {
-                throw "Houve um erro na API!";
-            }
-        })
-        .catch(function (resposta) {
-            console.error(resposta);
-        });
-}
-
-function atualizarDadosProcesso() {
-    fetch(`/redeMaquina/listarProcessos/${idMaquina}`)
-        .then(function (resposta) {
-            if (resposta.ok) {
-                resposta.json().then(function (resposta) {
-                    console.log("Dados recebidos: ", JSON.stringify(resposta));
-
-                    var feed = document.getElementById("feed_container");
-                    feed.innerHTML = "";
-
-                    if (resposta.length > 0) {
-                        for (let i = 0; i < resposta.length; i++) {
-                            const dados = resposta[i];
-                            const total_processos = dados.total_processos;
-                            const total_threads = dados.total_threads;
-                        
-                            document.getElementById("total-processos").innerHTML = `<span>${total_processos}</span>`;
-                            document.getElementById("total-threads").innerHTML = `<span>${total_threads}</span>`;
                     
-                        }
-                    } else {
-                        var mensagem = document.createElement("span");
-                        mensagem.innerHTML = "Nenhum resultado encontrado.";
-                        feed.appendChild(mensagem);
-                        throw "Nenhum resultado encontrado!";
-                    }
-
-                    atualizarDadosProcesso();
-
                 });
             } else {
                 throw "Houve um erro na API!";
@@ -218,6 +181,45 @@ function atualizarDadosProcesso() {
             console.error(resposta);
         });
 }
+
+// function atualizarDadosProcesso() {
+//     fetch(`/redeMaquina/listarProcessos/${idMaquina}`)
+//         .then(function (resposta) {
+//             if (resposta.ok) {
+//                 resposta.json().then(function (resposta) {
+//                     console.log("Dados recebidos: ", JSON.stringify(resposta));
+
+//                     var feed = document.getElementById("feed_container");
+//                     feed.innerHTML = "";
+
+//                     if (resposta.length > 0) {
+//                         for (let i = 0; i < resposta.length; i++) {
+//                             const dados = resposta[i];
+//                             const total_processos = dados.total_processos;
+//                             const total_threads = dados.total_threads;
+                        
+//                             document.getElementById("total-processos").innerHTML = `<span>${total_processos}</span>`;
+//                             document.getElementById("total-threads").innerHTML = `<span>${total_threads}</span>`;
+                    
+//                         }
+//                     } else {
+//                         var mensagem = document.createElement("span");
+//                         mensagem.innerHTML = "Nenhum resultado encontrado.";
+//                         feed.appendChild(mensagem);
+//                         throw "Nenhum resultado encontrado!";
+//                     }
+
+                    
+
+//                 });
+//             } else {
+//                 throw "Houve um erro na API!";
+//             }
+//         })
+//         .catch(function (resposta) {
+//             console.error(resposta);
+//         });
+// }
 
 
 atualizarDadosCluster();
